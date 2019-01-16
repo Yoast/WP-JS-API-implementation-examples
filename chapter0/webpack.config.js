a8a41a6fdce9dc7c30cb9e4459fe92d8719c3497
Jimmy Comack <jimmy@yoast.com>
2019-01-16 15:17:47 +0100

    Fixed initial setup by removing old references from an older project

--- chapter0/webpack.config.js
+++ chapter0/webpack.config.js
@@ -3,18 +3,13 @@ const path = require( "path" );
 module.exports = {
 	mode: process.env.NODE_ENV,
 	entry: {
-		pagebuilder: "./js/src/pagebuilder.js",
+		index: "./index.js",
 	},
 	output: {
 		path: path.resolve( "js", "dist" ),
 		filename: "[name].js",
 	},
 	externals: {
-		"@wordpress/data": "wp.data",
-		"@wordpress/element": "wp.element",
-		"@wordpress/compose": "wp.compose",
-		"@wordpress/plugins": "wp.plugins",
-		"lodash": "window.lodash",
 	},
 	module: {
 		rules: [
