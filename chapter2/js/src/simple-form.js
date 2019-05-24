bfb40fb61c20d56934daf07be9d0b5bd2d32d21d
Jimmy Comack <jimmy@yoast.com>
2019-05-24 10:38:33 +0200

    Fixed code samples for jQuery

--- chapter2/js/src/simple-form.js
+++ chapter2/js/src/simple-form.js
@@ -8,23 +8,6 @@ const store = createStore(
 );
 
 $( function () {
-	var form = "<div>" +
-	     "<section>" +
-	        "<label for='title'>Title</label><input type='text' id='title' name='title' />" +
-	     "</section>" +
-	     "<section>" +
-	        "<label for='slug'>Slug</label><input type='text' id='slug' name='slug'/>" +
-	     "</section>" +
-	     "<section>" +
-	        "<label for='content'>Content</label><textarea name='content' id='content' cols='60' rows='10'></textarea>" +
-	     "</section>" +
-	     "<section>" +
-             "<label for='excerpt'>Excerpt</label><textarea name='excerpt' id='excerpt' cols='60' rows='10'></textarea>" +
-	     "</section>" +
-     "</div>";
-
-	$( "#root" ).append( form );
-
 	// Attach events
 	$( '#title' ).on( 'keyup', function() {
 		store.dispatch( setFormTitle( this.value ) );
