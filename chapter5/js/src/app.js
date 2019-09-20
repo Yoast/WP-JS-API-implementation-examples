8fcd199768e89fb4dd9892ac43f81208ed1ad72d
Jimmy Comack <jimmy@yoast.com>
2019-09-20 14:16:24 +0200

    Moved line of code

--- chapter5/js/src/app.js
+++ chapter5/js/src/app.js
@@ -5,8 +5,8 @@ import editorReducer from "./reducers/core-editor";
 import * as editorActions from "./actions/core-editor";
 import * as editorSelectors from "./selectors/core-editor";
 
-import {combineReducers, registerStore} from "@wordpress/data";
 import Editor from "./components/Editor";
+import {combineReducers, registerStore} from "@wordpress/data";
 
 registerStore( "core/editor", {
 	reducer: combineReducers( { editor: editorReducer } ),
